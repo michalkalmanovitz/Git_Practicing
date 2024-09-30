2a44f3c43e8ffcddebffc0cdec325c78a947af62
michalkalmanovitz <michal.kalmanovitz@gmail.com>
2024-09-30 12:44:16 +0300

    add input for adress and name

--- self_practicing.py
+++ self_practicing.py
@@ -1,3 +1,14 @@
 from Const import NAME
 print("hello", NAME)
-print("Yali Nahum, Hamadia")
+
+#####################
+#####################
+
+#1
+name = input(" Hello Enter your  name: ")
+
+#2
+adress = input(" Hello Enter your adress: ")
+
+
+print(" Hello " , name ," \nYour adress: ", adress )
